22776a8f2dbc748181f364d0bfb14f5fa90c764b
zhou <47948826+zhouLife@users.noreply.github.com>
2024-06-27 00:30:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -59,7 +59,7 @@ CONFIG_PACKAGE_luci-app-linkease=y
 " >> .config
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
